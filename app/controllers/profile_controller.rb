74926e37ba9ae2b3b428dbe463bab1fff16f9886
sdkevorkian <sdkevorkian@gmail.com>
2017-06-02 11:16:02 -0700

    profile edit is working thanks jackson

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,5 +4,19 @@ class ProfileController < ApplicationController
   end
 
   def edit
+    @user = User.find(@current_user[:id])
   end
+
+  def update
+    u = User.find(@current_user[:id])
+    u.update(user_params)
+    redirect_to '/profile/show'
+  end
+
+ private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :name, :zip_code, :has_daily, :has_weekly, :has_monthly, :notification_type, :notification_freq)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'profile/edit' => 'profile#edit'
 
+post 'profile/edit' => 'profile#update'
+  # post 'profile/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
 
