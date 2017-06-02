87313aa45c64abe92519e33f5993cbaaea3ef345
sdkevorkian <sdkevorkian@gmail.com>
2017-06-01 22:46:46 -0700

    made api call return 16days

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,7 +2,7 @@ require 'httparty'
 class WeatherController < ApplicationController
 
   def show
-      @response = HTTParty.get('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=442d640bc37e3509f97eb9305a2905f8&zip=98155')
+      @response = HTTParty.get('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=442d640bc37e3509f97eb9305a2905f8&zip=98155&cnt=16')
       puts @response
   end
 end
