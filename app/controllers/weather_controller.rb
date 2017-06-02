3000d5b42d3c5887749bb54dce32c2a0723ceae3
sdkevorkian <sdkevorkian@gmail.com>
2017-06-01 18:10:35 -0700

    tested httparty and got it working on a weather route

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,8 @@
+require 'httparty'
+class WeatherController < ApplicationController
+
+  def show
+      @response = HTTParty.get('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=442d640bc37e3509f97eb9305a2905f8&zip=98155')
+      puts @response
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
 
+  #weather test
+  get 'weather' => 'weather#show'
   # signup routes
   get 'signup' => 'signup#new'
   post 'signup' => 'signup#create'
